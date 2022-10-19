1379e6c0f009914d803601aebdfa54943f97419f
Ahmad Elsagheer <ahmad.muhammad.elsagheer@gmail.com>
2022-10-19 13:09:45 +0200

    add workflows

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 # remove all containers
 docker rm -f $(docker ps -a -q)
 # Pull & Run
-docker pull us-central1-docker.pkg.dev/learn-terraform-364715/docker-images/pys:latest
-docker run -d -p 80:80 --name pys-container us-central1-docker.pkg.dev/learn-terraform-364715/docker-images/pys:latest
+docker pull "$1"
+docker run -d -p 80:80 --name pys-container "$1"
--- gcloud-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $(dirname "$0")
-
-PROJECT="learn-terraform-364715"
-ZONE="us-central1-c"
-
-gcloud compute scp ./deploy.sh root@py-server:~/deploy.sh --zone=$ZONE --project=$PROJECT
-gcloud compute ssh root@py-server --zone=$ZONE --project=$PROJECT -- "~/deploy.sh"
-
